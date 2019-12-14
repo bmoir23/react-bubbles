2fc3ad5d55ca126a8c9a43eb866475b1a59db773
bmoir23 <YOUR@EMAIL.com>
2019-12-13 23:34:24 -0500

    MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "./axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,20 +21,45 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    let activeColor = colors.filter(color => color.id === colorToEdit.id);
+
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${activeColor[0].id}`, colorToEdit)
+    .then(res => {
+      colors = colors.filter(color => color.id !== res.data.id)
+      updateColors([...colors, res.data])
+      setEditing(false)
+    })
+    .catch(err =>
+      console.log(err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete( `http://localhost:5000/api/colors/${color.id}`)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+    axiosWithAuth()
+    .get(`http://localhost:5000/api/colors`)
+    .then((res) => {
+      updateColors(res.data)
+    })
+
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
+      <p>Click on a color to save or delete!</p>
+      <p><strong>For more colors go to <a href="https://www.color-hex.com/"> https://www.color-hex.com/</a> and add your own!</strong></p>
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={e => {
+                    // come back here 
                     e.stopPropagation();
                     deleteColor(color)
                   }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,63 @@
-import React from "react";
 
-const Login = () => {
+import React from "react";
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const LoginForm = ({errors, touched}) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+   <Form className='loginForm'>
+     <h1>Welcome to the Bubble App!</h1>
+     <p>Login</p>
+     <div>
+       {touched.username && errors.username && <p>{errors.username}</p>}
+       < Field className='field'
+               type='text'
+               name='username'
+               placeholder='User Name...'
+               />
+     </div>
+     
+     <div>
+       {touched.userPassword && errors.userPassword && <p>{errors.userPassword}</p>}
+       < Field className='field'
+               type='password'
+               name='userPassword'
+               placeholder='password...'
+               />
+     </div>
+     <button type='submit'>Submit</button>
+   </Form>
   );
 };
+
+const Login = withFormik({
+  mapPropsToValues({ username, userPassword }) {
+    return {
+      username: username || "",
+      userPassword: userPassword || ""
+    };
+  },
+
+ validationSchema: Yup.object().shape({
+   username: Yup.string()
+   .required( 'User Name is Required'),
+   userPassword: Yup.string()
+   .required('Password is Required')
+ }) ,
+
+ handleSubmit(values, {props}) {
+   axios.post('http://localhost:5000/api/login', {
+     username: values.username,
+     password: values.userPassword
+   })
+   .then(res => {
+     localStorage.setItem('token', res.data.payload);
+     props.history.push('/bubblePage')
+   })
+ }
+})(LoginForm)
 
 export default Login;
